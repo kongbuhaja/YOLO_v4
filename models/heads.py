0ce43b0feaffc1837774df17c659ca066c1a1505
kongbuhaja <pcar530@gmail.com>
2024-02-07 22:38:35 +0900

    update Detect

--- models/heads.py
+++ models/heads.py
@@ -6,19 +6,19 @@ from tensorflow.keras.initializers import HeUniform as he
 from models.blocks import ConvLayer
 
 class Detect(Layer):
-    def __init__(self, decode, scales, row_anchors, col_anchors, num_classes, kernel_initializer=glorot):
+    def __init__(self, decode, row_anchors, col_anchors, num_classes, kernel_initializer=glorot):
         super().__init__()
         self.decode = decode
+        self.col_anchors = col_anchors
         self.detects = []
         for l in range(row_anchors):
-            self.detects += [[ConvLayer(col_anchors * (num_classes + 5), 1, activate=False, bn=False, kernel_initializer=kernel_initializer),
-                              Reshape((*scales[l][::-1], col_anchors, num_classes + 5))]]
+            self.detects += [ConvLayer(col_anchors * (num_classes + 5), 1, activate=False, bn=False, kernel_initializer=kernel_initializer)]
 
     @tf.function
     def call(self, x, training=False):
         branch = []
         for l in range(len(self.detects)):
-            r = self.detects[l][1](self.detects[l][0](x[l], training))
-            branch += [self.decode(r)]
+            r = self.detects[l][0](x[l], training)
+            branch += [self.decode(tf.reshape(r, [*r.shape[:3], self.col_anchors, -1]))]
 
         return branch
