5cc7a7c08f761ee3352b47faafd2cbc15c0578f4
kongbuhaja <pcar530@gmail.com>
2024-02-07 22:46:03 +0900

    update sppblock

--- models/blocks.py
+++ models/blocks.py
@@ -209,10 +209,10 @@ class SPPBlock(Layer):
         super().__init__()
         self.conv1 = ConvLayer(unit, 1, activate=activate, kernel_initializer=kernel_initializer)
         self.conv2 = ConvLayer(unit*2, 3, activate=activate, kernel_initializer=kernel_initializer)
-        self.conv3 = ConvLayer(unit, 1, activate=activate, kernel_initializer=kernel_initializer)
         self.spp = SPPLayer(unit, activate=activate, kernel_initializer=kernel_initializer)
+        self.conv3 = ConvLayer(unit, 1, activate=activate, kernel_initializer=kernel_initializer)
         self.conv4 = ConvLayer(unit*2, 3, activate=activate, kernel_initializer=kernel_initializer)
-        self.conv5 = ConvLayer(unit*2, 1, activate=activate, kernel_initializer=kernel_initializer)
+        self.conv5 = ConvLayer(unit, 1, activate=activate, kernel_initializer=kernel_initializer)
 
     @tf.function
     def call(self, x, training):
@@ -229,12 +229,12 @@ class CSPSPPBlock(Layer):
     def __init__(self, unit, activate='Mish', kernel_initializer=glorot, **kwargs):
         super().__init__()
         self.branch_transition = ConvLayer(unit, 1, activate=activate, kernel_initializer=kernel_initializer)
+        
         self.conv1 = ConvLayer(unit, 1, activate=activate, kernel_initializer=kernel_initializer)
         self.conv2 = ConvLayer(unit, 3, activate=activate, kernel_initializer=kernel_initializer)
-        self.conv3 = ConvLayer(unit, 1, activate=activate, kernel_initializer=kernel_initializer)
         self.spp = SPPLayer(unit, activate=activate, kernel_initializer=kernel_initializer)
-        self.conv4 = ConvLayer(unit, 1, activate=activate, kernel_initializer=kernel_initializer)
-        self.conv5 = ConvLayer(unit, 3, activate=activate, kernel_initializer=kernel_initializer)
+        self.conv3 = ConvLayer(unit, 1, activate=activate, kernel_initializer=kernel_initializer)
+        self.conv4 = ConvLayer(unit, 3, activate=activate, kernel_initializer=kernel_initializer)
 
         self.concat = Concatenate()
         self.concat_transition = ConvLayer(unit, 1, activate=activate, kernel_initializer=kernel_initializer)
@@ -245,10 +245,9 @@ class CSPSPPBlock(Layer):
 
         x = self.conv1(x, training)
         x = self.conv2(x, training)
-        x = self.conv3(x, training)
         x = self.spp(x, training)
+        x = self.conv3(x, training)
         x = self.conv4(x, training)
-        x = self.conv5(x, training)
 
         x = self.concat([x, branch])
         x = self.concat_transition(x, training)
