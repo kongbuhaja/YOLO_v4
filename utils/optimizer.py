1bbcf8d1d7339b04d2c1cd1bde70784fba898182
kongbuhaja <pcar530@gmail.com>
2024-03-14 15:48:53 +0900

    update optimizers

---
+++ utils/optimizer.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+from tensorflow.keras.optimizers import SGD, Adam
+
+class Optimizer():
+    def __init__(self, opt):
+        if opt['name'] in ['sgd', 'SGD']:
+            self.optimizer = SGD(momentum=opt['momentum'],
+                                 decay=opt['decay'])
+        elif opt['name'] in ['adam', 'Adam']:
+            self.optimizer = Adam(beta_1=opt['momentum'],
+                                  decay=opt['decay'])
+
+        self.lr = float(self.optimizer.lr.numpy())
+
+    def __call__(self, zip_data):
+        self.optimizer.apply_gradients(zip_data)
+
+    def assign_lr(self, lr):
+        self.optimizer.lr.assign(lr)
+        self.lr = lr
