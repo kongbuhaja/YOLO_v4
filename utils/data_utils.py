b0beb447611d2b246d1af8e996bd51f9b2fe8637
kongbuhaja <pcar530@gmail.com>
2024-03-19 00:46:16 +0900

    restore tf28

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -103,8 +103,9 @@ class DataLoader():
         batch_mosaic_images = []
         batch_mosaic_labels = []
         s = np.sqrt(size).astype(np.int32)
-        mosaic_size = self.input_size * 2
-        ix1, iy1, ix2, iy2 = *(mosaic_size//size), *(mosaic_size//size*(size-1))
+        mosaic_size = tf.round(self.input_size * 1.5)
+        ix1, iy1 = tf.round((mosaic_size - self.input_size)/2)
+        ix2, iy2 = ix1+self.input_size[0], iy1+self.input_size[1]
         
         for image, labels in data:
             batch_images += [image]
