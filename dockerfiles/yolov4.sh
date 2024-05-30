0e7f09658944bdaacfa6eda938b73fcb1e4a84ad
kongbuhaja <pcar530@gmail.com>
2024-05-30 01:47:06 +0000

    update dockerfiles shell

--- dockerfiles/yolov4.sh
+++ dockerfiles/yolov4.sh
@@ -1,7 +1,4 @@
-# sudo docker build --force-rm -f dockerfiles/yolov4.dockerfile -t yolov4:latest .
+sudo docker build --force-rm -f yolov4.dockerfile -t yolov4:latest .
 # sudo apt-get install x11-xserver-utils
 # xhost +
-# sudo docker run --gpus '"device=0"' --cpuset-cpus=0-7 -m 64g --shm-size=8g -it -v /home/dblab/ML/YOLO_v4:/home/YOLO_v4 --name yolov4_0 yolov4:latest
-sudo docker run --gpus '"device=1"' --cpuset-cpus=8-15 -m 64g --shm-size=8g -it -v /home/dblab/ML/YOLO_v4:/home/YOLO_v4 --name yolov4_1 yolov4:latest
-sudo docker run --gpus '"device=2"' --cpuset-cpus=16-23 -m 64g --shm-size=8g -it -v /home/dblab/ML/YOLO_v4:/home/YOLO_v4 --name yolov4_2 yolov4:latest
-# sudo docker run --gpus all --cpuset-cpus=0-31 -m 250g --shm-size=32g -it -v /tmp/.x11-unix:/tmp/.x11-unix -v /home/dblab/ML/YOLO_v4:/home/YOLO_v4 -p 6006:6006 -e DISPLAY=unix$DISPLAY --name yolov4 yolov4:latest
+sudo docker run --gpus '"device=0,1,2"' --cpuset-cpus=32-63 -m 128g --shm-size=16g -it -v /home/dblab/ML/YOLO_v4:/home/YOLO_v4 -p 6006:6006 --name yolov4 yolov4:tf28
